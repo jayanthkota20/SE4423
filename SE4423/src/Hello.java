72f6eb4f3230e61f3f32eee24013215dbac29cbe
jayanth kota <jkota@uco.edu>
2024-11-08 12:15:17 -0600

    README file added

--- SE4423/src/Hello.java
+++ SE4423/src/Hello.java
@@ -7,7 +7,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-
+		System.out.println("the conflict is solved");
+		System.out.println("a bug is fixed on bug fixing branch");
 	}
 
 }
